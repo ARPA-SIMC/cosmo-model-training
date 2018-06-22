0567745fb783fca25611452dc2044fab12e859d1
Davide Cesari <dcesari@arpae.it>
2018-06-22 18:10:45 +0200

    update of grib_api tool

--- tools/convert_def.sh
+++ tools/convert_def.sh
@@ -6,70 +6,112 @@
 # definitions for grib_api 1.12.3.
 # Bash shell strictly required.
 # To be run in the directory containing definitions.edzw/ subdirectory.
-# Two command line arguments can be provided: the numeric code and the
-# textual abbrevation of the non-DWD center respectively, default 80
-# and cnmc for Italy if not provided.
+# Two command line arguments can be provided: the numeric code and
+# optionally the textual abbrevation of the non-DWD center
+# respectively.
 
-from=78
-to=${1:-80}
-totext=${2:-cnmc}
-if [ ! -d "definitions.edzw/" ]; then
-    echo "Error, this script has to be executed in the directory containing"
-    echo "definitions.edzw/ subdirectory"
-    exit 1
-fi
-
-# fix odd file permissions
-find definitions.edzw/ -type d -exec chmod 775 \{\} \;
-find definitions.edzw/ -type f -exec chmod 664 \{\} \;
-cp -a definitions.edzw/ definitions.$totext
-
-cd definitions.$totext/grib1
-
-set -x
+fix_grib1() {
+    cd definitions.$totext/grib1
 
 # localDefinitionNumber.$from.table can probably not be copied, taken
 # care of in section1.def
 # cleanup
-rm -f local.$to.def local.$to.253.def local.$to.254.def \
- localDefinitionNumber.$to.table
-for file in local.$from.def local.$from.253.def local.$from.254.def \
-    localDefinitionNumber.$from.table; do
-    ln -s $file ${file/$from/$to}
-done
+    rm -f local.$to.def local.$to.253.def local.$to.254.def \
+	localDefinitionNumber.$to.table
+    for file in local.$from.def local.$from.253.def local.$from.254.def \
+	localDefinitionNumber.$from.table; do
+	ln -s $file ${file/$from/$to}
+    done
 
 # cleanup section.1.def
-rm -f section.1.def.$to
-[ -h section.1.def ] && rm -f section.1.def
-[ -f section.1.def.$from -a ! -f section.1.def ] && mv section.1.def.$from section.1.def
+    rm -f section.1.def.$to
+    [ -h section.1.def ] && rm -f section.1.def
+    [ -f section.1.def.$from -a ! -f section.1.def ] && mv section.1.def.$from section.1.def
 
-# edit section.1.def, needed for 1.12.3
-if grep -q "centre=$from;" section.1.def; then
-    mv section.1.def section.1.def.$from
-    sed -e "s/centre=$from;/centre=$to;/g" section.1.def.$from > section.1.def.$to
-    ln -s section.1.def.$to section.1.def
-fi
-
+# edit section.1.def, needed for >=1.12.3
+    if grep -q "centre=$from;" section.1.def; then
+	mv section.1.def section.1.def.$from
+	sed -e "s/centre=$from;/centre=$to;/g" section.1.def.$from > section.1.def.$to
+	ln -s section.1.def.$to section.1.def
+    fi
 
 # cleanup localConcepts/ and local/
-rm -rf localConcepts/$totext local/$totext
+    rm -rf localConcepts/$totext
+    rm -f local/$totext
 
 # replicate localConcepts/
-if [ -f localConcepts/edzw/stepType.def ]; then
+    if [ -f localConcepts/$fromtext/stepType.def ]; then
 # since version 1.13.1 DWD stepType is (re)defined in localConcepts
 # under condition centre==78, so we need to copy and edit
-    cp -a localConcepts/edzw localConcepts/$totext
+	cp -a localConcepts/$fromtext localConcepts/$totext
 # maybe we can omit centre? to be tested
-#    sed -e "s/centre=$from;//g" localConcepts/edzw/stepType.def > \
-    sed -e "s/centre=$from;/centre=$to;/g" localConcepts/edzw/stepType.def > \
-	localConcepts/$totext/stepType.def
-else
-    ln -s edzw localConcepts/$totext
-fi
+#    sed -e "s/centre=$from;//g" localConcepts/$fromtext/stepType.def > \
+	sed -e "s/centre=$from;/centre=$to;/g" localConcepts/$fromtext/stepType.def > \
+	    localConcepts/$totext/stepType.def
+    else
+	ln -s $fromtext localConcepts/$totext
+    fi
 # replicate local/
-if [ -d local ]; then
-    ln -s edzw local/$totext
+    if [ -d local ]; then
+	ln -s $fromtext local/$totext
+    fi
+
+    cd -
+}
+
+fix_grib2() {
+    cd definitions.$totext/grib2
+
+    rm -f grib2LocalSectionNumber.$to.table local.$to.def tables/local/$totext \
+	localConcepts/$totext
+    ln -s grib2LocalSectionNumber.$from.table grib2LocalSectionNumber.$to.table
+    ln -s local.$from.def local.$to.def
+    ln -s $fromtext tables/local/$totext
+    ln -s $fromtext localConcepts/$totext
+
+    cd -
+}
+
+
+usage() {
+    echo "Usage: `basename $0` <nationalcentre> [<textualcentre>]"
+}
+
+from=78
+fromtext=edzw
+#to=${1:-80}
+to=$1
+if [ -z "$to" ]; then
+    echo "Error, numerical code of national centre not provided"
+    usage
+    exit 1
 fi
 
-cd -
-echo "New definitions created in definitions.$totext"
+if [ ! -d "definitions.$fromtext/" ]; then
+    echo "Error, this script has to be executed in the directory containing"
+    echo "definitions.$fromtext/ subdirectory"
+    usage
+    exit 1
+fi
+
+# detect textual name of center, is this the correct way?
+totext=`awk "/^${to}/ {print \\$2}" definitions.$fromtext/grib2/local/2.0.table`
+#totext=${2:-cnmc}
+if [ -z "$totext" ]; then
+    echo "Error, cannot detect automatically the textual code of national centre"
+    echo "the numerical code $to will be used"
+    totext=$to
+fi
+# fix odd file permissions
+find definitions.$fromtext/ -type d -exec chmod 775 \{\} \;
+find definitions.$fromtext/ -type f -exec chmod 664 \{\} \;
+
+echo "Adapting $from ($fromtext) definitions to centre $to ($totext)..."
+
+cp -a definitions.$fromtext/ definitions.$totext
+fix_grib1
+fix_grib2
+
+echo "New definitions created in definitions.$totext."
+echo "Definitions in definitions.$totext are still compatible"
+echo "with original definitions for centre $from only for grib2, but not for grib1."
